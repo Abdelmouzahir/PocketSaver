6425d2fb5cb2dddb37a70e5a394665f80bf11e4b
abdel mouzahir <jalil.mouzahir@gmail.com>
2025-08-06 11:09:50 -0400

    update firebase URL

--- util/http.js
+++ util/http.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BACKEND_URL = "https://pocketsaver-df153-default-rtdb.firebaseio.com";
+const BACKEND_URL = "https://pocketsaver-df153-default-rtdb.firebaseio.com/";
 
 export default async function storeExp(expenseData){
    const response = await axios.post(BACKEND_URL + '/expenses.json', expenseData);
@@ -8,21 +8,28 @@ export default async function storeExp(expenseData){
    return id;
 }
 
-export async function fetchExpenses(){
-    const response = await axios.get( BACKEND_URL + '/expenses.json');
+export async function fetchExpenses() {
+  try {
+    const response = await axios.get(BACKEND_URL + '/expenses.json');
 
     const expenses = [];
-    for(const key in response.data){
-    const expenseObj = {
+    for (const key in response.data) {
+      const expenseObj = {
         id: key,
         amount: response.data[key].amount,
         date: new Date(response.data[key].date),
-        description: response.data[key].description
-    };
-    expenses.push(expenseObj); 
+        description: response.data[key].description,
+      };
+      expenses.push(expenseObj);
     }
+
     return expenses;
- }
+  } catch (error) {
+    console.error('❌ Error fetching expenses:', error.message);
+    throw new Error('Could not fetch expenses!');
+  }
+}
+
 
 export function updateExp(id, expenseData){
     return axios.put(BACKEND_URL + `/expenses/${id}.json`, expenseData);
